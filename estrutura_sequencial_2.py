651e346419c83f37753e6dabdbda3c22df5e2738
Francoschulze <francoschulze@gmail.com>
2022-04-28 16:44:10 -0300

    Envio padrão!

---
+++ estrutura_sequencial_2.py
@@ -0,0 +1,10 @@
+"""
+Faça um Programa que peça um número e então
+mostre a mensagem O número informado foi [número].
+"""
+
+# Solicitando o número com a função input
+numero = int(input('Digite um número: '))
+
+# Mostrando na tela formatando com f-strings
+print(f'O número informado foi {numero}. ')
