913fdce73e648cde1169f75896071b7287078f2e
Francoschulze <francoschulze@gmail.com>
2022-04-29 19:28:17 -0300

    Exercício 7

---
+++ estrutura_sequencial_7.py
@@ -0,0 +1,14 @@
+"""
+Faça um Programa que calcule a área de um quadrado,
+em seguida mostre o dobro desta área para o usuário.
+"""
+
+# Variável com valor pré-definido
+lado_quadrado = 6
+
+# Calculando a área
+# Lado * Lado
+area = lado_quadrado * lado_quadrado
+
+# Mostrando na tela o resultado do dobro do cálculo da área
+print(f'O dobro da área de lado {lado_quadrado} é: {area * 2} ')
