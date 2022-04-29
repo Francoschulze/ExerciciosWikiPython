847aab7a912d6c262b20cf54a9de8eca7b09c4c2
Francoschulze <francoschulze@gmail.com>
2022-04-29 11:36:49 -0300

    Exercício 4

--- estrutura_sequencial_5.py
+++ estrutura_sequencial_5.py
@@ -5,5 +5,5 @@ Faça um Programa que converta metros para centímetros.
 # Variável com o valor definido
 metro = 10
 
-# Efetuado o cálculo na formatação com f-strings
+# Efetuado o cálculo na formatação da f-strings
 print(f'O valor {metro} m convertido para centímetro é: {10 * 100} ')
