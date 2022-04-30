8dbffff7040d9b1c39c8af149de322acbfd6451f
Franco <francoschulze@gmail.com>
2022-04-29 21:55:52 -0300

    Exercício 3

--- estrutura_sequencial_3.py
+++ estrutura_sequencial_3.py
@@ -5,5 +5,5 @@ Faça um Programa que peça dois números e imprima a soma.
 numero_1 = int(input('Digite o primeiro número: '))
 numero_2 = int(input('Digite o segundo número: '))
 
-# Realizando os cálculos na formatação
+# Realizando os cálculos na formatação com f-strings
 print(f'O resultado da soma de {numero_1} + {numero_2} = {numero_1 + numero_2} ')
