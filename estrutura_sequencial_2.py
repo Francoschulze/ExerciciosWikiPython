f573d511fd656f8d568e043d964861ecaea5d0ac
Franco <francoschulze@gmail.com>
2022-04-29 21:55:07 -0300

    Exercício 2

--- estrutura_sequencial_2.py
+++ estrutura_sequencial_2.py
@@ -3,7 +3,7 @@ Faça um Programa que peça um número e então
 mostre a mensagem O número informado foi [número].
 """
 
-# Solicitando o número com a função input
+# Solicitando os dados com a função input
 numero = int(input('Digite um número: '))
 
 # Mostrando na tela formatando com f-strings
